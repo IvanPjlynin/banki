0bf1fad0de1f246cdf73f78f67b072fd614945f4
IVAN POLUNIN <ipolynin@gmail.com>
2022-06-27 20:20:07 +0700

    Update index.php

--- templates/vsebanki/index.php
+++ templates/vsebanki/index.php
@@ -62,7 +62,7 @@ $stickyHeader = $this->params->get('sticky-header') ? 'position-sticky sticky-to
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <jdoc:include type="metas" />
     <jdoc:include type="styles" />
-    <jdoc:include type="scripts" />
+
 
 </head>
 
@@ -259,6 +259,7 @@ $stickyHeader = $this->params->get('sticky-header') ? 'position-sticky sticky-to
     </a>
     <?php endif; ?>
     <jdoc:include type="modules" name="debug" style="none" />
+    <jdoc:include type="scripts" />
 </body>
 
 </html>
