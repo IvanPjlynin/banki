b26f21310f6273e82cf297025a39b089787bd6e9
IVAN POLUNIN <ipolynin@gmail.com>
2022-07-02 23:07:49 +0700

    Update template.js

--- templates/vsebanki/js/template.js
+++ templates/vsebanki/js/template.js
@@ -1044,7 +1044,7 @@ jQuery(document).ready(function ($) {
                     <tr class="${trClassShowHide}">
                         <td><b>${i} платеж</b> <br>${moment().add('months', i).format('MMMM YYYY')}</th>
                         <td></td>
-                        <td></td>
+                        <td>${this.digits_int2(this.params["pereplata"]/this.params["srock-mec"]*i)}</td>
                         <td>${this.digits_int2(this.params["mes-plateg"]*i)}</td>
                         <td>${this.digits_int2(this.params["mes-plateg"])}</td>
                     </tr>`);
