dd82b07dcf3a94770cc85ede5b14a5c97b7e5e12
IVAN POLUNIN <ipolynin@gmail.com>
2022-07-03 22:30:03 +0700

    Update yrvote.php

--- plugins/content/yrvote/yrvote.php
+++ plugins/content/yrvote/yrvote.php
@@ -14,7 +14,8 @@ class plgContentYrvote extends JPlugin {
     protected $homepage = false;
 
     public function onContentPrepare($context, &$article, &$params, $limitstart = 0) {
-
+        print_r($context);
+        die();
         $content = $this->check_if_content($context, $article);
         if ($content == false)
             return false;
