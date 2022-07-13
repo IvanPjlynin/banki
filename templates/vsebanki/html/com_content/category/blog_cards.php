061f7b71b56cbff0ae388c394f672d6836b039a5
IVAN POLUNIN <ipolynin@gmail.com>
2022-07-13 10:52:43 +0700

    Update blog_cards.php

--- templates/vsebanki/html/com_content/category/blog_cards.php
+++ templates/vsebanki/html/com_content/category/blog_cards.php
@@ -52,7 +52,7 @@ $isUnpublished = ($this->item->state == ContentComponent::CONDITION_UNPUBLISHED 
       ?>
             <div class="bank-field summa">
                 <div class="bank-field-name"><?php echo $this->item->extrafields['kreditnyj-limit']->title; ?></div>
-                <div class="bank-field-value"><?php echo number_format($this->item->extrafields['kreditnyj-limit']->value, 0, ',', ' '); ?></div>
+                <div class="bank-field-value"><?php echo number_format($this->item->extrafields['kreditnyj-limit']->value, 0, ',', ' '); ?> ₽</div>
             </div>
             <div class="bank-field srok">
                 <div class="bank-field-name"><?php echo $this->item->extrafields['lgotnyj-period']->title; ?></div>
