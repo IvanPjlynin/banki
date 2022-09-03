f5fbdaf73b7e5e3a101b9caaeb7659507592a638
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-04 00:07:34 +0700

    Update credit-nalichnumi.php

--- templates/vsebanki/html/mod_jlcontentfieldsfilter/credit-nalichnumi.php
+++ templates/vsebanki/html/mod_jlcontentfieldsfilter/credit-nalichnumi.php
@@ -55,11 +55,11 @@ if ($params->get('enable_css', 1)) {
 
         <div class="col-md-3 block-filter">
             <div class="dropdown">
-                <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-expanded="false">
+                <a class="btn btn-secondary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                     Dropdown link
                 </a>
 
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                <ul class="dropdown-menu">
                     <li><a class="dropdown-item" href="#">Action</a></li>
                     <li><a class="dropdown-item" href="#">Another action</a></li>
                     <li><a class="dropdown-item" href="#">Something else here</a></li>
