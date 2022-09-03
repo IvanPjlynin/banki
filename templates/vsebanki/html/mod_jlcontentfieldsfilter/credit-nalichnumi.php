57d77e82779a7f0a2458dd8c8f224a9bd3b68e4f
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-03 19:09:15 +0700

    Update credit-nalichnumi.php

--- templates/vsebanki/html/mod_jlcontentfieldsfilter/credit-nalichnumi.php
+++ templates/vsebanki/html/mod_jlcontentfieldsfilter/credit-nalichnumi.php
@@ -7,7 +7,6 @@
  * @copyright	(C) 2017-2019 Arkadiy Sedelnikov, Joomline. All rights reserved.
  * @license 	GNU General Public License version 2 or later; see	LICENSE.txt
  */
-die();
 
 defined('_JEXEC') or die;
 $doc = JFactory::getDocument();
