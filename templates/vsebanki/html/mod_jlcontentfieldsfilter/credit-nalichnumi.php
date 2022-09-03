e816cb5a1d34870e2ff0ddcba76703dec4bc5363
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-04 00:25:55 +0700

    -

--- templates/vsebanki/html/mod_jlcontentfieldsfilter/credit-nalichnumi.php
+++ templates/vsebanki/html/mod_jlcontentfieldsfilter/credit-nalichnumi.php
@@ -55,8 +55,8 @@ if ($params->get('enable_css', 1)) {
 
         <div class="col-md-3 block-filter">
             <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown button
+                <button class="btn dropdown-toggle filter-dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                    Фильтры поиска
                 </button>
                 <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
                     <li><a class="dropdown-item" href="#">Action</a></li>
