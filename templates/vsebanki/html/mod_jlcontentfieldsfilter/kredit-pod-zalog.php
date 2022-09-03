234655705b038a9de182b7a57f267f55a348348a
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-04 03:06:15 +0700

    Update kredit-pod-zalog.php

--- templates/vsebanki/html/mod_jlcontentfieldsfilter/kredit-pod-zalog.php
+++ templates/vsebanki/html/mod_jlcontentfieldsfilter/kredit-pod-zalog.php
@@ -147,11 +147,11 @@ if ($params->get('enable_css', 1)) {
         var $input_filter_range1 = $("#input-credit-range-one");
         var instance_filter_range1;
 
-        $("#summa-kredita-ot-from-109").trigger("keypress").val(function(i, val) {
+        $("#summa-kredita-ot-from-137").trigger("keypress").val(function(i, val) {
             return 0;
         });
 
-        $("#summa-kredita-ot-to-109").trigger("keypress").val(function(i, val) {
+        $("#summa-kredita-ot-to-137").trigger("keypress").val(function(i, val) {
             return 500000;
         });
 
@@ -177,7 +177,7 @@ if ($params->get('enable_css', 1)) {
             max: 5000000,
             onStart: function(data) {
                 $input_filter_range1.prop("value", data.from);
-                $("#summa-kredita-ot-to-109").trigger("keypress").val(function(i, val) {
+                $("#summa-kredita-ot-to-137").trigger("keypress").val(function(i, val) {
                     return data.from;
                 });
                 $("#summa-kredita-from-109").trigger("keypress").val(function(i, val) {
@@ -186,7 +186,7 @@ if ($params->get('enable_css', 1)) {
             },
             onChange: function(data) {
                 $input_filter_range1.prop("value", data.from);
-                $("#summa-kredita-ot-to-109").trigger("keypress").val(function(i, val) {
+                $("#summa-kredita-ot-to-137").trigger("keypress").val(function(i, val) {
                     return data.from;
                 });
                 $("#summa-kredita-from-109").trigger("keypress").val(function(i, val) {
@@ -200,7 +200,7 @@ if ($params->get('enable_css', 1)) {
         $input_filter_range1.on("input", function() {
             var value = $(this).prop("value");
 
-            $("#summa-kredita-ot-to-109").trigger("keypress").val(function(i, val) {
+            $("#summa-kredita-ot-to-137").trigger("keypress").val(function(i, val) {
                 return value;
             });
             $("#summa-kredita-from-109").trigger("keypress").val(function(i, val) {
