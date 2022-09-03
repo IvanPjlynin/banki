e44cbc1ae2caceb6b8cfa2779fd626e5b9bb5342
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-03 22:57:36 +0700

    Update template.js

--- templates/vsebanki/js/template.js
+++ templates/vsebanki/js/template.js
@@ -472,8 +472,12 @@ jQuery(document).ready(function ($) {
     var $input_filter_range1 = $("#input-credit-range-one");
     var instance_filter_range1;
 
-    $("#summa-kredita-ot-from-109").val(0).trigger("input");
-    $("#summa-kredita-ot-to-109").val(500000).trigger("input");
+    $("#summa-kredita-ot-from-109").trigger("keypress").val(function (i, val) {
+        return 0;
+    });
+    $("#summa-kredita-ot-to-109").trigger("keypress").val(function (i, val) {
+        return 500000;
+    });
 
     $filter_range1.ionRangeSlider({
         type: "single",
@@ -487,11 +491,15 @@ jQuery(document).ready(function ($) {
         max: 5000000,
         onStart: function (data) {
             $input_filter_range1.prop("value", data.from);
-            $("#summa-kredita-ot-to-109").val(data.from).trigger("input");
+            $("#summa-kredita-ot-to-109").trigger("keypress").val(function (i, val) {
+                return 500000;
+            });
         },
         onChange: function (data) {
             $input_filter_range1.prop("value", data.from);
-            $("#summa-kredita-ot-to-109").val(data.from).trigger("input");
+            $("#summa-kredita-ot-to-109").trigger("keypress").val(function (i, val) {
+                return 500000;
+            });
         }
     });
 
