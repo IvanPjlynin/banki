d298cc7454eecb044d3e8fbaab906c3ce0d9062e
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-04 00:20:00 +0700

    Update index.php

--- templates/vsebanki/index.php
+++ templates/vsebanki/index.php
@@ -58,9 +58,11 @@ $stickyHeader = $this->params->get('sticky-header') ? 'position-sticky sticky-to
 <html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
 
 <head>
+
+    <jdoc:include type="metas" />
+    <jdoc:include type="scripts" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <jdoc:include type="metas" />
     <jdoc:include type="styles" />
 
 
@@ -259,7 +261,7 @@ $stickyHeader = $this->params->get('sticky-header') ? 'position-sticky sticky-to
     </a>
     <?php endif; ?>
     <jdoc:include type="modules" name="debug" style="none" />
-    <jdoc:include type="scripts" />
+
 </body>
 
 </html>
