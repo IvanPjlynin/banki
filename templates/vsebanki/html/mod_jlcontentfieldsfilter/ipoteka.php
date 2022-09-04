d1d4207b4d7684eeffd8f5afb6437367919e3ef8
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-04 12:07:04 +0700

    Update ipoteka.php

--- templates/vsebanki/html/mod_jlcontentfieldsfilter/ipoteka.php
+++ templates/vsebanki/html/mod_jlcontentfieldsfilter/ipoteka.php
@@ -230,7 +230,7 @@ if ($params->get('enable_css', 1)) {
         $filter_range2.ionRangeSlider({
             type: "single",
             grid: false,
-            from: 72,
+            from: 60,
             postfix: '',
             min: 12,
             step: 1,
