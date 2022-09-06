763d14d377d5ac87b032b04a980d3f4d6adc9079
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-06 20:23:20 +0700

    Update credit.php

--- templates/vsebanki/html/com_content/article/credit.php
+++ templates/vsebanki/html/com_content/article/credit.php
@@ -252,6 +252,24 @@ $attribs['style'] = 'none';
                             </div>
                             <?php endif; ?>
 
+                            <?php if ($this->item->extrafields['zarplata_credits']->value) : ?>
+                            <div class="col-12 col-sm-12 col-md-4 col-lg-4 tab-text-block mt-2">
+                                <h3 class="tab-text-block-title">Зарплата</h3>
+                                <p class="tab-text-block-content">
+                                    <?php echo $this->item->extrafields['zarplata_credits']->value; ?>
+                                </p>
+                            </div>
+                            <?php endif; ?>
+
+                            <?php if ($this->item->extrafields['trebovaniya_k_zalogu']->value) : ?>
+                            <div class="col-12 col-sm-12 col-md-4 col-lg-4 tab-text-block mt-2">
+                                <h3 class="tab-text-block-title">Требования к залогу</h3>
+                                <p class="tab-text-block-content">
+                                    <?php echo $this->item->extrafields['trebovaniya_k_zalogu']->value; ?>
+                                </p>
+                            </div>
+                            <?php endif; ?>
+
                         </div>
                     </div>
                     <div class="tab-pane fade" id="tab-dopolnitelno-tab" role="tabpanel" aria-labelledby="tab-dopolnitelno">
