e92c37279ef566f8b817f25766ffc584b2b1eb25
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-06 15:46:14 +0700

    Update credit.php

--- templates/vsebanki/html/com_content/article/credit.php
+++ templates/vsebanki/html/com_content/article/credit.php
@@ -175,7 +175,17 @@ $attribs['style'] = 'none';
                             <?php if ($this->item->extrafields['stavka-bez-strahov-ot']->value) : ?>
                             <div class="col-12 col-sm-12 col-md-6 col-lg-3 tab-text-block">
                                 <h3 class="tab-text-block-title">Ставка без страховки</h3>
-                                <p class="tab-text-block-content"><?php echo $this->item->extrafields['stavka-bez-strahov-ot']->value; ?>% - <?php echo $this->item->extrafields['stavka-bez-strahov-do']->value; ?>%</p>
+                                <p class="tab-text-block-content">
+                                    
+                                <?php if (!$this->item->extrafields['stavka-bez-strahov-do']->value) : ?>
+                                  от 
+                                <?php endif; ?>                                                 
+                                                            
+                                <?php echo $this->item->extrafields['stavka-bez-strahov-ot']->value; ?>%
+                                
+                                <?php if ($this->item->extrafields['stavka-bez-strahov-do']->value) : ?>
+                                  - <?php echo $this->item->extrafields['stavka-bez-strahov-do']->value; ?>%</p>
+                                <?php endif; ?>
                             </div>
                             <?php endif; ?>
 
