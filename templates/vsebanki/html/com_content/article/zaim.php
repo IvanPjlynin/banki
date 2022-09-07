b215b660d174466b5a1c0010959dcfc647bf028f
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-07 14:39:08 +0700

    Update zaim.php

--- templates/vsebanki/html/com_content/article/zaim.php
+++ templates/vsebanki/html/com_content/article/zaim.php
@@ -189,7 +189,7 @@ $attribs['style'] = 'none';
                             <div class="col-12 col-sm-12 col-md-6 col-lg-3 tab-text-block">
                                 <h3 class="tab-text-block-title">Ставка</h3>
                                 <p class="tab-text-block-content">
-                                    <?php if (!$this->item->extrafields['stavka-zajma-do']->value) : ?>
+                                    <?php if ($this->item->extrafields['stavka-zajma-do']->value == 'нет') : ?>
                                         от <?php echo $this->item->extrafields['stavka-zajma']->value; ?>%
                                     <? else: ?>
                                         <?php echo $this->item->extrafields['stavka-zajma']->value; ?>% - <?php echo $this->item->extrafields['stavka-zajma-do']->value; ?>%
