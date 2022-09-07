65769d79f84c8dd6c04f0e651cbdd209ad982e82
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-07 11:41:58 +0700

    Update zaim.php

--- templates/vsebanki/html/com_content/article/zaim.php
+++ templates/vsebanki/html/com_content/article/zaim.php
@@ -179,17 +179,17 @@ $attribs['style'] = 'none';
                             </div>
                             <?php endif; ?>
 
+                            <?php if ($this->item->extrafields['stavka-zajma']->value) : ?>
+                            <div class="col-12 col-sm-12 col-md-6 col-lg-3 tab-text-block">
+                                <h3 class="tab-text-block-title">Ставка</h3>
+                                <p class="tab-text-block-content"><?php echo $this->item->extrafields['stavka-zajma']->value; ?>% - <?php echo $this->item->extrafields['stavka-zajma-do']->value; ?>%</p>
+                            </div>
+                            <?php endif; ?>
+                            
                             <?php if ($this->item->extrafields['srok-zajma']->value) : ?>
                             <div class="col-12 col-sm-12 col-md-6 col-lg-3 tab-text-block">
                                 <h3 class="tab-text-block-title">Срок</h3>
                                 <p class="tab-text-block-content"><?php echo $this->item->extrafields['srok-zajma-ot']->value; ?> дн. - <?php echo $this->item->extrafields['srok-zajma']->value; ?> дн.</p>
-                            </div>
-                            <?php endif; ?>
-
-                            <?php if ($this->item->extrafields['stavka-zajma']->value) : ?>
-                            <div class="col-12 col-sm-12 col-md-6 col-lg-3 tab-text-block">
-                                <h3 class="tab-text-block-title">Ставка cо страховкой</h3>
-                                <p class="tab-text-block-content"><?php echo $this->item->extrafields['stavka-zajma']->value; ?>% - <?php echo $this->item->extrafields['stavka-zajma-do']->value; ?>%</p>
                             </div>
                             <?php endif; ?>
 
