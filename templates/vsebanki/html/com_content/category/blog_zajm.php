c7f5808530e0ffbfab41af050263a59f4d81810a
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-07 12:54:50 +0700

    Update blog_zajm.php

--- templates/vsebanki/html/com_content/category/blog_zajm.php
+++ templates/vsebanki/html/com_content/category/blog_zajm.php
@@ -56,7 +56,7 @@ $isUnpublished = ($this->item->state == ContentComponent::CONDITION_UNPUBLISHED 
             </div>
             <div class="bank-field srok">
                 <div class="bank-field-name"><?php echo $this->item->extrafields['srok-zajma']->title; ?></div>
-                <div class="bank-field-value">до <?php echo $this->item->extrafields['srok-zajma']->value; ?> мес.</div>
+                <div class="bank-field-value">до <?php echo $this->item->extrafields['srok-zajma']->value; ?> дн.</div>
             </div>
             <div class="bank-field stavka">
                 <div class="bank-field-name"><?php echo $this->item->extrafields['stavka-zajma']->title; ?></div>
