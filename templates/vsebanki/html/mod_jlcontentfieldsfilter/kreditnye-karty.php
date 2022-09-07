d213e8202622fb1894c77d077e32d76e9861d65e
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-07 15:37:15 +0700

    Update kreditnye-karty.php

--- templates/vsebanki/html/mod_jlcontentfieldsfilter/kreditnye-karty.php
+++ templates/vsebanki/html/mod_jlcontentfieldsfilter/kreditnye-karty.php
@@ -152,7 +152,7 @@ if ($params->get('enable_css', 1)) {
         });
 
         $("#kreditnyj-limit-to-142").trigger("keypress").val(function(i, val) {
-            return 3000000;
+            return 1000000;
         });
 
 
@@ -165,7 +165,7 @@ if ($params->get('enable_css', 1)) {
             postfix: '',
             step: 1000,
             grid_snap: '',
-            max: 3000000,
+            max: 1000000,
             onStart: function(data) {
                 $input_filter_range1.prop("value", data.from);
                 $("#kreditnyj-limit-from-142").trigger("keypress").val(function(i, val) {
