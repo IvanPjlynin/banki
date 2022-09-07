0cc031b52bb5b568fe6ec777a4ebc56f18414e2e
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-07 16:21:52 +0700

    Update kreditnye-karty.php

--- templates/vsebanki/html/mod_jlcontentfieldsfilter/kreditnye-karty.php
+++ templates/vsebanki/html/mod_jlcontentfieldsfilter/kreditnye-karty.php
@@ -86,14 +86,7 @@ if ($params->get('enable_css', 1)) {
                         </label>
                     </div>
 
-                    <hr>
-
-                    <div class="col-md-12 block-filter">
-                        <h4>Смогу подтвердить доход справкой 2-НДФЛП или справкой по форме банка?</h4>
-                        <label class="jlmf-sublabel switch" for="podtverzhdenie-dokhoda-109">Да <input type="checkbox" value="1" id="podtverzhdenie-dokhoda-109" name="jlcontentfieldsfilter[41]" class="jlmf-checkbox">
-                            <span class="slider round"></span>
-                        </label>
-                    </div>
+                    
 
 
                 </div>
