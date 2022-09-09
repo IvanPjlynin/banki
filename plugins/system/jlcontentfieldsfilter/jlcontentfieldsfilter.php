eb1ce37896ee482f73107ef7cd7fa32d5a380d09
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-09 14:34:13 +0700

    Update jlcontentfieldsfilter.php

--- plugins/system/jlcontentfieldsfilter/jlcontentfieldsfilter.php
+++ plugins/system/jlcontentfieldsfilter/jlcontentfieldsfilter.php
@@ -100,7 +100,7 @@ class plgSystemJlContentFieldsFilter extends JPlugin
             }
             $catid = $app->getUserStateFromRequest($option.'.jlcontentfieldsfilter.tag_category_id', 'tag_category_id', 0, 'int');
             $tagids = $app->getUserStateFromRequest($option.'.jlcontentfieldsfilter.tag_ids', 'id', array(), 'array');
-            $itemid = implode(',', $tagids) . ':' . $app->input->get('Itemid', 0, 'int');
+            //$itemid = implode(',', $tagids) . ':' . $app->input->get('Itemid', 0, 'int');
         }
 		else if(!in_array($option, array('com_content', 'com_contact')) || $view != 'category' || $catid == 0)
 		{
