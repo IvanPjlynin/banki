3520e83e0c692da8ed331818d15bab7033b4510b
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-09 15:18:18 +0700

    Update mod_jlcontentfieldsfilter.php

--- modules/mod_jlcontentfieldsfilter/mod_jlcontentfieldsfilter.php
+++ modules/mod_jlcontentfieldsfilter/mod_jlcontentfieldsfilter.php
@@ -109,3 +109,6 @@ if(count($fields)){
 	require JModuleHelper::getLayoutPath('mod_jlcontentfieldsfilter', $params->get('layout', 'default'));
 }
 
+
+$app->setUserState($context . 'filter.article_id_include', null);
+$app->setUserState($context . 'filter.article_id', null);
