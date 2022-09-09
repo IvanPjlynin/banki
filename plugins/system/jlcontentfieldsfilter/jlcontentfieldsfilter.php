e5c52b1a14edc6b687d4445ce6cb7db3b49d8ada
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-09 15:00:28 +0700

    Update jlcontentfieldsfilter.php

--- plugins/system/jlcontentfieldsfilter/jlcontentfieldsfilter.php
+++ plugins/system/jlcontentfieldsfilter/jlcontentfieldsfilter.php
@@ -119,7 +119,7 @@ class plgSystemJlContentFieldsFilter extends JPlugin
 
 		$filterData = $app->getUserStateFromRequest($context, 'jlcontentfieldsfilter', array(), 'array');
 
-        var_dump($filterData);
+        
         
 		if(!count($filterData))
 		{
