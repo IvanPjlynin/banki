22aaf94015e8be0a7a33bd02efa5a9f9caff7cdb
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-11 17:50:29 +0700

    Update credit-nalichnumi.php

--- templates/vsebanki/html/mod_jlcontentfieldsfilter/credit-nalichnumi.php
+++ templates/vsebanki/html/mod_jlcontentfieldsfilter/credit-nalichnumi.php
@@ -246,7 +246,7 @@ if ($params->get('enable_css', 1)) {
             step: 1,
             grid_num: 4,
             grid_snap: '',
-            max: 2000,
+            max: 240,
             onStart: function(data) {
                 $input_filter_range2.prop("value", data.from);
                 $("#srok-ot-to-109").trigger("keypress").val(function(i, val) {
