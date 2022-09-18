578750879b0a6d49b1b13dbc55d262799c539ea6
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-18 16:05:21 +0700

    Update kredit-na-avtomobil.php

--- templates/vsebanki/html/mod_jlcontentfieldsfilter/kredit-na-avtomobil.php
+++ templates/vsebanki/html/mod_jlcontentfieldsfilter/kredit-na-avtomobil.php
@@ -360,7 +360,7 @@ if ($params->get('enable_css', 1)) {
         //подсчет чебоксов
         $('.dropdown-menu .jlmf-checkbox').change(function() {
             var n = $(".dropdown-menu input:checked").length;
-            $('button.filter-dropdown-toggle span').html('(+' + n + ')');
+            $('button.filter-dropdown-toggle span').html('(+' + (4 - n) + ')');
         });
 
 
