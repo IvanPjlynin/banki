974ac88b61ed2f0a82d3ada69a5e1376480fe0d1
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-19 16:40:08 +0700

    Update jlcontentfieldsfilter.php

--- plugins/system/jlcontentfieldsfilter/jlcontentfieldsfilter.php
+++ plugins/system/jlcontentfieldsfilter/jlcontentfieldsfilter.php
@@ -197,6 +197,7 @@ class plgSystemJlContentFieldsFilter extends JPlugin
                     
                 echo '<pre>';
                 print_r($fieldsTypes[$k]);
+                print_r($v);
                 echo '</pre>';
                     
                     //ivp если значение равно 1 то включаем в массив исключение
