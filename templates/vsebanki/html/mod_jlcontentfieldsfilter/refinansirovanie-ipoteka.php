0443b663a1320c47d3a7d646e33493b37dfaa3b4
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-19 13:46:54 +0700

    Update refinansirovanie-ipoteka.php

--- templates/vsebanki/html/mod_jlcontentfieldsfilter/refinansirovanie-ipoteka.php
+++ templates/vsebanki/html/mod_jlcontentfieldsfilter/refinansirovanie-ipoteka.php
@@ -345,3 +345,26 @@ if ($params->get('enable_css', 1)) {
     });
 
 </script>
+<style>
+    .blog .tabsmenu a:nth-child(2)~div,
+    .blog .tabsmenu a:nth-child(2):hover~div {
+        transform: translate(135px);
+        -webkit-transform: translate(135px);
+        background: #56C182;
+        transition: all .33s linear;
+        -webkit-transition: all .33s linear;
+        width: 200px;
+        color: #002D4F;
+    }
+
+    .blog .tabsmenu a:nth-child(1):hover~div {
+        transform: translate(0px);
+        -webkit-transform: translate(0px);
+        background: #56C182;
+        transition: all .33s linear;
+        -webkit-transition: all .33s linear;
+        width: 85px;
+        color: #002D4F;
+    }
+
+</style>
