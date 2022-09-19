e928ae27c24b53926197c0cdad3a075fb5a8ca7f
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-19 16:08:12 +0700

    Update kredit-pod-zalog.php

--- templates/vsebanki/html/mod_jlcontentfieldsfilter/kredit-pod-zalog.php
+++ templates/vsebanki/html/mod_jlcontentfieldsfilter/kredit-pod-zalog.php
@@ -402,9 +402,10 @@ if ($params->get('enable_css', 1)) {
             }
         });
 
+
         var allVars = $.getUrlVars();
         var getZalogUrl = $.getUrlVar('jlcontentfieldsfilter%5B47%5D');
-        console.log('URL', allVars, getZalogUrl);
+        //console.log('URL', allVars, getZalogUrl);
 
         //если займ
         if (getZalogUrl > 0) {
