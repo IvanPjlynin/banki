e44523902c2ea678705e8ce2b0a04e42427b30a3
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-26 09:42:49 +0700

    Update vclad.php

--- templates/vsebanki/html/com_content/article/vclad.php
+++ templates/vsebanki/html/com_content/article/vclad.php
@@ -177,7 +177,28 @@ $attribs['style'] = 'none';
                                 <p class="tab-text-block-content">от <?php echo number_format($this->item->extrafields['summa-vklada']->value, 0, ',', ' '); ?> ₽</p>
                             </div>
                             <?php endif; ?>
-
+                            
+                            <?php if ($this->item->extrafields['stavka-vklada']->value) : ?>
+                            <div class="col-12 col-sm-12 col-md-3 col-lg-3 tab-text-block">
+                                <h3 class="tab-text-block-title">Ставка</h3>
+                                <p class="tab-text-block-content">до <?php echo $this->item->extrafields['stavka-vklada']->value; ?> %</p>
+                            </div>
+                            <?php endif; ?>
+                            
+                            <?php if ($this->item->extrafields['srok-vklada-ot']->value) : ?>
+                            <div class="col-12 col-sm-12 col-md-3 col-lg-3 tab-text-block">
+                                <h3 class="tab-text-block-title">Срок</h3>
+                                <p class="tab-text-block-content">
+                                    от <?php echo $this->item->extrafields['stavka-vklada']->value; ?> мес.
+                                    
+                                    <?php if ($this->item->extrafields['srok-vklada-do']->value && $this->item->extrafields['srok-vklada-do']->value !== '999' ) : ?>
+                                        до <?php echo $this->item->extrafields['srok-vklada-do']->value; ?> мес.
+                                    <?php endif; ?>                         
+                                                                
+                                    
+                                </p>
+                            </div>
+                            <?php endif; ?>
                             
 
                         </div>
