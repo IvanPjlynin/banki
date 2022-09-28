0da3ff46934a895fa8352b6a6d4389d2e892adae
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-28 16:53:45 +0700

    Update template.js

--- templates/vsebanki/js/template.js
+++ templates/vsebanki/js/template.js
@@ -663,6 +663,23 @@ jQuery(document).ready(function ($) {
       
     });
     
+    //получаем GET параметры в url
+    
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    };
+    
+    console.log('getUrlParameter', getUrlParameter);
+    
     
     //разбитие цены на разряды
     //$('.summa .bank-field-value').each(function (i, elem) {
