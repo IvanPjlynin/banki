0c17c1af53b9613a986fef642f485292c5b97dd2
IVAN POLUNIN <ipolynin@gmail.com>
2022-09-28 16:59:37 +0700

    -

--- templates/vsebanki/html/mod_jlcontentfieldsfilter/credit-nalichnumi.php
+++ templates/vsebanki/html/mod_jlcontentfieldsfilter/credit-nalichnumi.php
@@ -374,6 +374,27 @@ if ($params->get('enable_css', 1)) {
             var n = $(".dropdown-menu input:checked").length;
             $('button.filter-dropdown-toggle span').html('(+' + (4 - n) + ')');
         });
+        
+        //получаем GET параметры в url
+        var getUrlParameter = function getUrlParameter(sParam) {
+            var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+                sURLVariables = sPageURL.split('&'),
+                sParameterName,
+                i;
+            for (i = 0; i < sURLVariables.length; i++) {
+                sParameterName = sURLVariables[i].split('=');
+                if (sParameterName[0] === sParam) {
+                    return sParameterName[1] === undefined ? true : sParameterName[1];
+                }
+            }
+        };
+
+        //console.log('getUrlParameter', getUrlParameter('summ'));
+        if(getUrlParameter('summ')){
+            instance_filter_range1.update({
+                    from: getUrlParameter('summ')
+                });
+        }
 
 
     });
--- templates/vsebanki/js/template.js
+++ templates/vsebanki/js/template.js
@@ -663,27 +663,7 @@ jQuery(document).ready(function ($) {
       
     });
     
-    //получаем GET параметры в url
     
-    var getUrlParameter = function getUrlParameter(sParam) {
-        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
-            sURLVariables = sPageURL.split('&'),
-            sParameterName,
-            i;
-        for (i = 0; i < sURLVariables.length; i++) {
-            sParameterName = sURLVariables[i].split('=');
-            if (sParameterName[0] === sParam) {
-                return sParameterName[1] === undefined ? true : sParameterName[1];
-            }
-        }
-    };
-    
-    console.log('getUrlParameter', getUrlParameter('summ'));
-    if(getUrlParameter('summ')){
-        instance_filter_range1.update({
-                from: getUrlParameter('summ')
-            });
-    }
     
     
     //разбитие цены на разряды
