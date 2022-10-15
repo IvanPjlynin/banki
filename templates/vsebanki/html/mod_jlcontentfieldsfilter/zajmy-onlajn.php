47f44c34da34abcf1ea30ddd93b3615b7615c887
IVAN POLUNIN <ipolynin@gmail.com>
2022-10-15 21:58:27 +0700

    Update zajmy-onlajn.php

--- templates/vsebanki/html/mod_jlcontentfieldsfilter/zajmy-onlajn.php
+++ templates/vsebanki/html/mod_jlcontentfieldsfilter/zajmy-onlajn.php
@@ -333,7 +333,7 @@ if ($params->get('enable_css', 1)) {
 
         $filter_vozrast.ionRangeSlider({
             skin: "round",
-            type: "double",
+            type: "single",
             grid: false,
             from: 22,
             to: 65,
