cec2bb9f534c96a7511b55c390d884652d85e9b2
IVAN POLUNIN <ipolynin@gmail.com>
2022-10-15 19:48:57 +0700

    Update kredit-pod-zalog.php

--- templates/vsebanki/html/mod_jlcontentfieldsfilter/kredit-pod-zalog.php
+++ templates/vsebanki/html/mod_jlcontentfieldsfilter/kredit-pod-zalog.php
@@ -346,7 +346,7 @@ if ($params->get('enable_css', 1)) {
 
         $filter_vozrast.ionRangeSlider({
             skin: "round",
-            type: "double",
+            type: "single",
             grid: false,
             from: 22,
             to: 65,
@@ -379,7 +379,7 @@ if ($params->get('enable_css', 1)) {
             });
 
             $("#vozrast-dlya-pogasheniya-from-137").trigger("keypress").val(function(i, val) {
-                return to;
+                return from;
             });
         }
 
