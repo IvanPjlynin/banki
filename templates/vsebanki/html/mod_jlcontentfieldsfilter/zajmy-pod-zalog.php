95a4f4e225e062b0536207ac1981fa35129e875e
IVAN POLUNIN <ipolynin@gmail.com>
2022-10-15 22:11:34 +0700

    Update zajmy-pod-zalog.php

--- templates/vsebanki/html/mod_jlcontentfieldsfilter/zajmy-pod-zalog.php
+++ templates/vsebanki/html/mod_jlcontentfieldsfilter/zajmy-pod-zalog.php
@@ -149,6 +149,13 @@ if ($params->get('enable_css', 1)) {
 
 <script>
     jQuery(document).ready(function($) {
+
+        //разделитель на 1 000
+        function number_format(num, format) {
+            num = (num + "").replace(/(\s)+/g, "");
+            return format ? num.replace(/(\d{1,3})(?=(?:\d{3})+$)/g, "$1 ") : num
+        }
+
         //сумма кредита
         var $filter_range1 = $("#credit-range-one");
         var $input_filter_range1 = $("#input-credit-range-one");
@@ -183,7 +190,7 @@ if ($params->get('enable_css', 1)) {
             grid_snap: '',
             max: 6000000,
             onStart: function(data) {
-                $input_filter_range1.prop("value", data.from);
+                $input_filter_range1.prop("value", number_format(data.from, true));
                 $("#summa-zajma-ot-to-141").trigger("keypress").val(function(i, val) {
                     return data.from;
                 });
@@ -192,7 +199,7 @@ if ($params->get('enable_css', 1)) {
                 });
             },
             onChange: function(data) {
-                $input_filter_range1.prop("value", data.from);
+                $input_filter_range1.prop("value", number_format(data.from, true));
                 $("#summa-zajma-ot-to-141").trigger("keypress").val(function(i, val) {
                     return data.from;
                 });
@@ -204,18 +211,22 @@ if ($params->get('enable_css', 1)) {
 
         instance_filter_range1 = $filter_range1.data("ionRangeSlider");
 
-        $input_filter_range1.on("input", function() {
+        $input_filter_range1.on("focus", function() {
+            this.value = number_format(this.value, true);
+            this.focus();
+            this.selectionStart = this.value.length
+        }).on("input", function() {
             var value = $(this).prop("value");
 
             $("#summa-zajma-ot-to-141").trigger("keypress").val(function(i, val) {
-                return value;
+                return number_format(value);
             });
             $("#summa-zajma-from-141").trigger("keypress").val(function(i, val) {
-                return value;
+                return number_format(value);
             });
 
             instance_filter_range1.update({
-                from: value
+                from: number_format(value)
             });
         });
 
