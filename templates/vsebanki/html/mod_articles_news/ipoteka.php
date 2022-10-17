4c09bcecfdcf1a65b093b5bc24936307998610a4
IVAN POLUNIN <ipolynin@gmail.com>
2022-10-17 15:44:06 +0700

    Update ipoteka.php

--- templates/vsebanki/html/mod_articles_news/ipoteka.php
+++ templates/vsebanki/html/mod_articles_news/ipoteka.php
@@ -18,16 +18,16 @@ if (!$list)
 
 ?>
 <div class="mod-articlesnews newsflash row eq">
-	<?php foreach ($list as $item) : ?>
-		<div class="col-12 col-sm-12 col-md-4 mod-articlesnews__item" itemscope itemtype="https://schema.org/Article">
-			<div class="article-wrapper">
-			   <?php require ModuleHelper::getLayoutPath('mod_articles_news', '_item-ipoteka'); ?>
-			</div>
-		</div>
-	<?php endforeach; ?>
+    <?php foreach ($list as $item) : ?>
+    <div class="col-12 col-sm-12 col-md-4 mod-articlesnews__item" itemscope itemtype="https://schema.org/Article">
+        <div class="article-wrapper">
+            <?php require ModuleHelper::getLayoutPath('mod_articles_news', '_item-ipoteka'); ?>
+        </div>
+    </div>
+    <?php endforeach; ?>
 </div>
 <div class="row">
-   <div class="col-12 col-sm-12 col-md-4 offset-md-4">
-      <a class="articles-button" href="ipoteka/refinansirovanie" target="_blank">Все предложения</a>
-   </div>
+    <div class="col-12 col-sm-12 col-md-4 offset-md-4">
+        <a class="articles-button" href="/ipoteka/master-podbora-ipoteki" target="_blank">Все предложения</a>
+    </div>
 </div>
