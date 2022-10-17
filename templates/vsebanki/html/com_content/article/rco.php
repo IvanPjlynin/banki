056d0fd7aec487524fed534da020ed168b516ccd
IVAN POLUNIN <ipolynin@gmail.com>
2022-10-17 13:12:23 +0700

    Update rco.php

--- templates/vsebanki/html/com_content/article/rco.php
+++ templates/vsebanki/html/com_content/article/rco.php
@@ -120,7 +120,10 @@ $attribs['style'] = 'none';
                     <?php echo $this->item->extrafields['rejting']->value; ?>
                 </div>
                 <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 column bank-logo">
-                    <?php echo LayoutHelper::render('joomla.content.full_image', $this->item); ?>
+                    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                        <?php echo LayoutHelper::render('joomla.content.full_image', $this->item); ?>
+                    </div>
+
                     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                         <h4><?php echo $this->item->extrafields['rco-name']->value; ?></h4>
                     </div>
