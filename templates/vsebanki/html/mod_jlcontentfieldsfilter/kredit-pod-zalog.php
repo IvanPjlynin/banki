103b4edb83551cc7b6ee57ba01a4b820cc4b4e55
IVAN POLUNIN <ipolynin@gmail.com>
2022-10-18 16:45:24 +0700

    Update kredit-pod-zalog.php

--- templates/vsebanki/html/mod_jlcontentfieldsfilter/kredit-pod-zalog.php
+++ templates/vsebanki/html/mod_jlcontentfieldsfilter/kredit-pod-zalog.php
@@ -456,6 +456,16 @@ if ($params->get('enable_css', 1)) {
 
 </script>
 <style>
+    .blog .tabsmenu a:nth-child(1):hover~div {
+        transform: translate(0px);
+        -webkit-transform: translate(0px);
+        background: #56C182;
+        transition: all .33s linear;
+        -webkit-transition: all .33s linear;
+        width: 120px;
+        color: #002D4F;
+    }
+
     .blog .tabsmenu a:nth-child(2)~div {
         transform: translate(170px);
         -webkit-transform: translate(170px);
