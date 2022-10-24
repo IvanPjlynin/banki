2bdc7240774e11702d6abed4ef56b1e196ea6aea
IVAN POLUNIN <ipolynin@gmail.com>
2022-10-24 19:53:19 +0700

    Update jlcontentfieldsfilter.php

--- plugins/system/jlcontentfieldsfilter/jlcontentfieldsfilter.php
+++ plugins/system/jlcontentfieldsfilter/jlcontentfieldsfilter.php
@@ -307,10 +307,10 @@ class plgSystemJlContentFieldsFilter extends JPlugin
 //ivp вычетаем исключенные материалы        
 $filterArticles = array_diff($filterArticles, $excludeFilterArticles);        
         
-/*echo '<pre>';            
+echo '<pre>';            
 var_dump($filterArticles);
 echo '</pre>';
-die();*/
+die();
 //$filterArticles = rsort($filterArticles);
 
 		$context = $option.'.category.list.' . $itemid;
