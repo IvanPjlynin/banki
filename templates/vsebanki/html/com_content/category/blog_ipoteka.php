ba5443d39fd580eca600b89e33a47e934ad3399b
IVAN POLUNIN <ipolynin@gmail.com>
2022-10-25 14:11:44 +0700

    Update blog_ipoteka.php

--- templates/vsebanki/html/com_content/category/blog_ipoteka.php
+++ templates/vsebanki/html/com_content/category/blog_ipoteka.php
@@ -30,13 +30,7 @@ $isUnpublished = ($this->item->state == ContentComponent::CONDITION_UNPUBLISHED 
 	|| ($this->item->publish_down < $currentDate && $this->item->publish_down !== null);
 
 ?>
-
-<div class="item-content row eq ipoteka">
-    <?php if ($isUnpublished) : ?>
-    <div class="system-unpublished">
-        <?php endif; ?>
-        <?php echo LayoutHelper::render('joomla.content.blog_style_default_item_title', $this->item); ?>
-        <?php
+<?php
          $this->item->extrafields = array();
          if (isset($this->item->jcfields) && is_array($this->item->jcfields)) {
             foreach ($this->item->jcfields as $field) {
@@ -46,6 +40,13 @@ $isUnpublished = ($this->item->state == ContentComponent::CONDITION_UNPUBLISHED 
 	        }
          }
       ?>
+
+<div class="item-content row eq ipoteka" data-summ='<?php echo $this->item->extrafields['summa-ipoteka']->value; ?>' data-stavka='<?php echo $this->item->extrafields['stavka-bez-strakhovki-ot-4']->value; ?>' data-sroc='<?php echo $this->item->extrafields['srok-2']->value; ?>' data-id='<?php echo $this->item->id; ?>'>
+    <?php if ($isUnpublished) : ?>
+    <div class="system-unpublished">
+        <?php endif; ?>
+        <?php echo LayoutHelper::render('joomla.content.blog_style_default_item_title', $this->item); ?>
+
         <div class="col-12 col-sm-12 col-md-3 column bank-logo">
             <?php echo LayoutHelper::render('joomla.content.intro_image', $this->item); ?>
             <h4 class="fs-4 mt-2"><?php echo $this->item->extrafields['nazvanie-produkta']->value; ?></h4>
