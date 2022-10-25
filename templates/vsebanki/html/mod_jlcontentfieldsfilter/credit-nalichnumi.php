454cd378428b8ca7b31c7d9ff868bc756f026354
IVAN POLUNIN <ipolynin@gmail.com>
2022-10-25 11:30:38 +0700

    Update credit-nalichnumi.php

--- templates/vsebanki/html/mod_jlcontentfieldsfilter/credit-nalichnumi.php
+++ templates/vsebanki/html/mod_jlcontentfieldsfilter/credit-nalichnumi.php
@@ -442,7 +442,9 @@ $assetManager->registerAndUseScript('tinysort', 'https://cdnjs.cloudflare.com/aj
 
 
 
-
+        jQuery('div.com-content-category-blog__items.blog-items>div.com-content-category-blog__item.blog-item').tinysort('div.com-content-category-blog__item.blog-item>div.item-content', {
+            attr: 'data-summ'
+        });
 
 
     });
